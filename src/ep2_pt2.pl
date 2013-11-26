91f50f5349f1954879eba639ea8d6100c394e7f9
Bruno Faria <nunes@outlook.com>
2013-11-25 22:21:26 -0200

    almost final. pt1, pt2 ok. pt3 almost

--- src/ep2_pt2.pl
+++ src/ep2_pt2.pl
@@ -1,66 +1,50 @@
-% monta_lista (-L)
-%   Reads a line of text, breaking it into a
-%   a list of atomic terms: [this,is,an,example].
 monta_lista(L):-
     leChar(X, T),
     completaLinha(X, T, L).
 
-% leChar(-Char,-Type)
-%  Reads a character and runs it through tipoChar/1.
 leChar(X,T):-
-    get0(C), % Gets a character from the current input source (check via 
-	     % seeing/1).
+    get0(C),
     tipoChar(C,T,X).
 
-% completaLinha(+FirstC,+FirstT,-L)
-%  Given FirstC (the first character) and FirstT (its type), reads
-%  and tokenizes the rest of the line into atoms and numbers.
-completaLinha(_, end, []):-!.	% stop at end
-completaLinha(_, blank, L):- % skip blanks
+completaLinha(_, end, []):-!.
+completaLinha(_, blank, L):-
     !,
     monta_lista(L).
-completaLinha(X, special, [A|L]):- 	% special char
+completaLinha(X, special, [A|L]):-
     !,
     name(A, [X]),
     monta_lista(L).
-completaLinha(X, ialpha,[A|[]]):-	% begin word
+completaLinha(X, ialpha,[A|[]]):-
     !,
     name(A, [X]), !.
     %monta_lista(L).
-completaLinha(X, alpha, [A|L]):-	% begin word
+completaLinha(X, alpha, [A|L]):-
     completaPalavra(X, alpha, W, C0, T0),
-    name(A, W),	% may have some problems with numbers
+    name(A, W),	
     completaLinha(C0, T0, L).
 
-% completaPalavra(+X,+FirstT,-List,-FollC,-FollT)
-%  Given X (the first character) and FirstT (its type)
-%  reads the rest of a word, putting its characters into List.
 completaPalavra(X,alpha,[X|List],X0,T0):-
     !,
     leChar(C1,T1),
     completaPalavra(C1,T1,List,X0,T0).
-completaPalavra(X,T0,[],X,T0). % where FirstT is not alpha
+completaPalavra(X,T0,[],X,T0). 
 
-% tipoChar(+Code,?Type,-NewCode)
-%  Given an ASCII Code classifies the character as 'end'
-%  (of line/file), 'blank', 'alpha'(numeric), or 'special',
-%  and changes it to a potentially different character (NewCode).
 tipoChar(10,end,10):- !.	% UNIX eol mark
 tipoChar(13,end,13):- !.	% DOS eol mark
-tipoChar(-1,end,-1):- !.	% get0 end of file code
+tipoChar(-1,end,-1):- !.	% get0 eof code
 tipoChar(46,ialpha,46):-!.  % '.'
 tipoChar(63,ialpha,63):-!.  % '?'
-tipoChar(Code,blank,32):-	% blanks, other CTRL codes
+tipoChar(Code,blank,32):-	% blanks, outros CTRL codes
     Code =< 32,
     !.
-tipoChar(Code,alpha,Code):-	% digits
+tipoChar(Code,alpha,Code):-	% digitos
     48 =< Code, Code =< 57,
     !.
-tipoChar(Code,alpha,Code):-	% lower-case letters
+tipoChar(Code,alpha,Code):-	% lower-case
     97 =< Code, Code =< 122,
     !.
-tipoChar(Code,alpha,NewCode) :- % upper-case letters
+tipoChar(Code,alpha,NewCode) :- % upper-case
 	65 =< Code, Code =< 90,
 	!,
-	NewCode is Code + 32. % translate to lower case
-tipoChar(Code,special,Code).	% all others
+	NewCode is Code + 32. % traduz para lower case
+tipoChar(Code,special,Code).	% todos os outros
